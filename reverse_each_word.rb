752a5bf8d403bfb09c08b93f2bb303a2e4642773
blackwell <blackwellsmithiv@gmail.com>
2019-01-22 00:19:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
-def reverse_each_word(st)
- array = phrase.split
- array.collect do |word| 
-   word.reverse
+def reverse_each_word(string)
+ original_array = string.split(" ")
+ return_array = []
+ original_array.each do |string|
+   return_array<< string.reverse
   end
-
+  return_array.join(" ")
 end 
